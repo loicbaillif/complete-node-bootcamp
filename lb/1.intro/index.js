7c118dccbb5319c8ce021ead5f063fbff64aa38c
loicbaillif <larmedeciel@gmail.com>
2025-07-26 20:32:05 +0100

    1.intro/fs/3 Write to file

--- lb/1.intro/index.js
+++ lb/1.intro/index.js
@@ -3,5 +3,9 @@ const fs = require('fs');
 const textInput = fs.readFileSync('./../../1-node-farm/starter/txt/input.txt', 'utf-8');
 console.log(textInput);
 
-const hello = "Hello, world!";
-console.log(hello);
+const textOutput = `This is what we know about the avocado: \n${textInput}\n
+Created on ${Date.now()}\n`;
+
+// write to a file
+fs.writeFileSync('./../../1-node-farm/starter/txt/output.txt', textOutput);
+console.log('File output.txt written.');
