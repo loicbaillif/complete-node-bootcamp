073a1704cb04b41e3274af3e51ff59ae116c10ee
loicbaillif <larmedeciel@gmail.com>
2025-07-26 20:27:20 +0100

    1.intro/fs/2 Read from file

--- lb/1.intro/index.js
+++ lb/1.intro/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-fs.readFileSync();
+const textInput = fs.readFileSync('./../../1-node-farm/starter/txt/input.txt', 'utf-8');
+console.log(textInput);
 
 const hello = "Hello, world!";
 console.log(hello);
