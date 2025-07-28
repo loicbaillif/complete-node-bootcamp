8b10ec05756530b7180fd3342dbcd45f634e74ea
loicbaillif <larmedeciel@gmail.com>
2025-07-28 20:54:22 +0100

    1.intro/fs/6 read file, async method

--- lb/1.intro/index.js
+++ lb/1.intro/index.js
@@ -6,6 +6,14 @@ console.log(textInput);
 const textOutput = `This is what we know about the avocado: \n${textInput}\n
 Created on ${Date.now()}\n`;
 
-// write to a file
+// write to a file (synchronous method)
 fs.writeFileSync('./../../1-node-farm/starter/txt/output.txt', textOutput);
 console.log('File output.txt written.');
+
+// Read/write to a file (asynchronous method)
+fs.readFile(
+    './../../1-node-farm/starter/txt/start.txt',
+    'utf-8',
+    (err, data) => {
+       console.log(data);
+    });
